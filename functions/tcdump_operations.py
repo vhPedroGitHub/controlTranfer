ffdeedb6ea8e46ba33dbd3ecc4cf1a5fd7a8e949
vhPedroGitHub <134577062+vhPedroGitHub@users.noreply.github.com>
2025-03-19 09:21:22 -0400

    cambios

--- functions/tcdump_operations.py
+++ functions/tcdump_operations.py
@@ -69,7 +69,7 @@ def is_program_installed(pod_name, namespace, v1, command, program):
 def run_tcpdump_in_pod(pod_name, namespace, v1):
     try:
         # Comando para ejecutar tcpdump en segundo plano
-        command = ["sh", "-c", f"tcpdump -Z root -i any -w /tmp/{pod_name}.pcap &"]
+        command = ["sh", "-c", f"tcpdump -Z root -w /tmp/{pod_name}.pcap &"]
         print(f"Iniciando tcpdump en el Pod {pod_name}...")
 
         # Ejecutar el comando en el Pod
